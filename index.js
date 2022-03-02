1155ffa06acdadca6b5481131bfd7058559dde51
Hector Colin <hector.colin@epita.fr>
2022-03-02 16:21:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const home = require('./routes/home');
+const staff = require('./routes/staff');
+const cookieParser = require('cookie-parser');
+const getNewsData = () => [
+    {
+      heading: 'Covid is Cured',
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      Auther: 'Una L\'Estrange'
+    },
+    {
+        heading: 'Covid is still here',
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        Auther: 'Una again',
+
+    }  
+]
+const newsMiddleware = (req, res, next) => {
+    if(!res.locals.partials) res.locals.partials = {}
+    res.locals.partials.newsContext = getNewsData()
+    next()
+}
+
+app.use(express.urlencoded({ extended: true })) 
+app.use(newsMiddleware)
+app.use(express.static('public'));
+app.use(cookieParser('Hector le fort'));
+
+// set up handlebars view engine
+var handlebars = require('express-handlebars')
+.create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use('/', home);
+app.use('/staff', staff);
+
+app.get('/technoblade',  (req, res) => {
+    res.type('text/plain');
+    res.send('Subscribe to Technoblade and donate to research against cancer');
+});
+
+// // 404 catch-all handler (middleware)
+app.use(function (req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+// // 500 error handler (middleware)
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ routes/staff.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+var data = {"foil" : { "name": "foil",
+        "dob": "01/01/1998",
+        "imageurl": "/images/foilimage1.png",
+        "hobbies": ["Jokes", "Gags", "Stand up", "Watch himself in a mirror and laugh", "Take care of Moustache"]},
+
+    "david" : { "name": "david",
+        "dob": "01/01/-100",
+        "imageurl": "/images/david.png",
+        "hobbies": ["Bully Goliath", "Take a shot"]},
+
+    "goliath" : { "name": "goliath",
+        "imageurl": "/images/goliath.png",
+        "hobbies": ["Eat", "Try to be scary"]
+}};
+
+
+router.get('/', (req,res) =>
+    res.render('listing', { listing: data }
+));
+
+router.get('/addnew', (req, res) =>
+{
+    var fname = req.query.firstname;
+    var sname = req.query.surname;
+    console.log('Date entered ' + fname + ' ' + sname)
+    res.render('personform')
+}
+)
+
+router.post('/addnew', (req, res) =>
+{
+    console.log("Data sent via post")
+    var fname = req.body.firstname;
+    var sname = req.body.surname;
+    console.log('Date entered ' + fname + ' ' + sname)
+    console.table(req.body)
+    res.redirect(303, 'personadded')
+}
+)
+
+router.get('/personadded', (req, res) => res.render('personadded'))
+
+router.get('/:name', (req, res) => {
+    var name = req.params.name;
+    if (name in data)
+    {
+        res.render('person', { person: data[name] })
+    }
+    else
+    {
+        res.status(404)
+        res.render('404')
+    }
+});
+
+module.exports = router;
