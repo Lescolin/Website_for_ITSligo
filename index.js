5fd96c7fd63d2b42231522f08cc69eb84dbe4d96
Hector Colin <hector.colin@epita.fr>
2022-03-02 17:07:19 +0000

    Customize about page

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const getNewsData = () => [
     {
       heading: 'Covid is Cured',
       body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-      Auther: 'Una L\'Estrange'
+      Auther: 'Hector Colin'
     },
     {
         heading: 'Covid is still here',
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        Auther: 'Una again',
+        Auther: 'Hector Colin',
 
     }  
 ]
